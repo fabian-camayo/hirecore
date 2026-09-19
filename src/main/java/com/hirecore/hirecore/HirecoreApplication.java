756ed1734926470184d2d8d0cd688646a7a0a379
Fabian Camayo <fabiancamayo365@gmail.com>
2026-09-19 18:55:34 -0500

    init project

---
+++ src/main/java/com/hirecore/hirecore/HirecoreApplication.java
@@ -0,0 +1,13 @@
+package com.hirecore.hirecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirecoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirecoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hirecore/hirecore/HirecoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirecore.hirecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirecoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
